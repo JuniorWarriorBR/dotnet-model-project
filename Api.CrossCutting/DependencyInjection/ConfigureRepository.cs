0926a183eacade72e8ba153b499304880f475b45
Junior <eujuniormesmo@gmail.com>
2020-05-08 00:54:11 -0300

    Depence Injection Configured

---
+++ Api.CrossCutting/DependencyInjection/ConfigureRepository.cs
@@ -0,0 +1,20 @@
+using Api.Data.Context;
+using Api.Data.Repository;
+using Api.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.CrossCutting.DependencyInjection
+{
+    public class ConfigureRepository
+    {
+        public static void ConfigureDependenciesRepository(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+
+            serviceCollection.AddDbContext<MyContext>(
+                options => options.UseMySql("Server=172.17.0.2;Port=3306;Database=dbAPI;Uid=root;Pwd=devRoot")
+            );
+        }
+    }
+}
