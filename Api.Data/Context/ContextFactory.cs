4b13e56f685643d99275d735e67f25720cf4f403
Junior <eujuniormesmo@gmail.com>
2020-05-08 00:06:03 -0300

    Model Created

---
+++ Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Api.Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            // Usado para criar as migrações
+            var connectionString = "Server=172.17.0.2;Port=3306;Database=dbAPI;Uid=root;Pwd=devRoot";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseMySql(connectionString);
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
